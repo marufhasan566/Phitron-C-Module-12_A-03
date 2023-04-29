41668e621a29201ca7b9193dae43ed6cef8ad663
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-29 12:27:33 +0600

    do the same

--- Do_The_Same.c
+++ Do_The_Same.c
@@ -3,6 +3,14 @@ int main()
 {
     int n, k;
     scanf("%d %d", &n, &k);
-    
+    for (int j = 0; j < k; j++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
